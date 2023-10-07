03d40c631d39328865d9a8c677bc4b9c45177467
zhiy <wuzhiyong1216@gmail.com>
2023-10-07 18:30:36 +0800

    实现关注、取消关注功能

--- community/src/main/resources/static/js/profile.js
+++ community/src/main/resources/static/js/profile.js
@@ -6,9 +6,34 @@ function follow() {
 	var btn = this;
 	if($(btn).hasClass("btn-info")) {
 		// 关注TA
-		$(btn).text("已关注").removeClass("btn-info").addClass("btn-secondary");
+		$.post(
+			CONTEXT_PATH + "/follow",
+			{"entityType":3, "entityId":$(btn).prev().val()},
+			function (data) {
+				data = $.parseJSON(data);
+				if (data.code == 0) {
+					window.location.reload();
+				} else {
+					alert(data.msg);
+				}
+			}
+		);
+		// 如果在这里更改样式,还需要同步修改关注和关注者的数量,为了方便起见,这里直接刷新整个页面
+		// $(btn).text("已关注").removeClass("btn-info").addClass("btn-secondary");
 	} else {
 		// 取消关注
-		$(btn).text("关注TA").removeClass("btn-secondary").addClass("btn-info");
+		$.post(
+			CONTEXT_PATH + "/unfollow",
+			{"entityType":3, "entityId":$(btn).prev().val()},
+			function (data) {
+				data = $.parseJSON(data);
+				if (data.code == 0) {
+					window.location.reload();
+				} else {
+					alert(data.msg);
+				}
+			}
+		);
+		// $(btn).text("关注TA").removeClass("btn-secondary").addClass("btn-info");
 	}
 }
