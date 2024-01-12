abbd9f2b247db29a93d355a7f8943f9063f5aa0a
zhiy <wuzhiyong1216@gmail.com>
2024-01-12 22:26:28 +0800

    feat:增加显示系统通知功能

--- community/src/main/java/com/nowcoder/community/config/WebMvcConfig.java
+++ community/src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.nowcoder.community.config;
 
 import com.nowcoder.community.controller.interceptor.LoginRequiredInterceptor;
 import com.nowcoder.community.controller.interceptor.LoginTicketInterceptor;
+import com.nowcoder.community.controller.interceptor.MessageInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -16,12 +17,19 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginRequiredInterceptor loginRequiredInterceptor;
 
+    @Autowired
+    private MessageInterceptor messageInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        // 对所有静态资源不拦截，对所有动态资源都拦截
         registry.addInterceptor(loginTicketInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.img", "/**/*.html", "/**/*.jpeg");
 
         registry.addInterceptor(loginRequiredInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.img", "/**/*.html", "/**/*.jpeg");
+
+        registry.addInterceptor(messageInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.img", "/**/*.html", "/**/*.jpeg");
     }
 }
